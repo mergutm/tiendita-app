d879a673825a779685354c278052c81848d055cc
mergutm <merg@gs.utm.mx>
2025-04-22 18:59:46 +0000

    adding Toy page

--- src/app/dashboard/toy/page.tsx
+++ src/app/dashboard/toy/page.tsx
@@ -1,7 +1,11 @@
+import { PokemonsReponse } from "@/app/pokemons/interfaces/pokemons-response";
+import { SimplePokemon } from "@/app/pokemons/interfaces/simple-pokemon";
+import Image from 'next/image';
 
-const respuesta = async (limit =5, offset = 0):Promise<SimplePokemon[]> => {
+const respuesta = async (limit =150, offset = 0):Promise<SimplePokemon[]> => {
     //console.log(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`);
-    const data: PokemonsReponse = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`)
+    const data: PokemonsReponse = await 
+        fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`)
     .then((response) => response.json())
 
     const pokemons = data.results.map(       
@@ -22,13 +26,16 @@ export default  async function ToyPage() {
     return (
       <div className="flex flex-col items-center justify-center w-full h-full">
             Pagina de pokemones   
-            <ul>
-            {
-                posts.results.map((post) => (
-                 <li key={post.name}>{post.url}</li>
-                ))
-            }
-      </ul>
+            <div className='flex flex-column'>
+              <div className="flex flex-wrap gap-10 items-center justify-center">
+                  <Image src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/18.svg`}
+                    width={150}
+                    height={150}
+                    alt = "imagen pokemon"
+                  />
+              </div>
+
+            </div>
       </div>
     );
   }
